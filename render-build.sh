41b0ed353d03de08ed82da33dccf9074d62e6bf5
Kunal Gambhir <129669697+kunalproject@users.noreply.github.com>
2025-04-04 15:44:37 +0530

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
+# Install Node.js dependencies
+npm install
+
 # Install Python and dependencies
 apt-get update
 apt-get install -y python3 python3-pip
 
 # Install Python packages
 pip3 install numpy pandas scikit-learn
-
-# Install Node.js dependencies
-npm install
